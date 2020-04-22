e074f37b36f8e1e020387185a304f1b06c29a035
Diego Souza <dee-souza@hotmail.com>
2020-04-22 14:43:42 -0300

    :art: Create service transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,36 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: string;
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError('O tipo da transação deve ser income ou outcome.');
+    }
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
